2c2a227301308b55bc62ed18f72145177b20d811
yuva <yuva@yuva-VGN-CS17G-R>
2012-11-20 15:43:07 +0530

    changes by yuva

--- table/dayone.php
+++ table/dayone.php
@@ -1,19 +1,37 @@
-<?php
-echo '<html><head><link rel="stylesheet" href="design.css" type="text/css"/></head><body>';
-echo "This is first file";
-$limit=$_POST['limit'];
-echo '<table id="tab">';
-for($i=1;$i<=$limit;$i++)	/* For loop for number of rows */
-{
-	echo '<tr height="10">';
-	for($j=1;$j<=$limit;$j++)	/* For loop for number of columns */
-	{
-		$a=$i*$j;
-		echo '<td>'.$a.'</td>';
+<!-- 
+	Edited by yuva after Imported from mugunthan
+  -->
+  
+<html>
 
-	}	
-	echo '</tr>';				/*End loop*/
-}
-echo '</table></body></html>';
-php?>
+	<head>
+		<link rel="stylesheet" href="design.css" type="text/css"/>
+	</head>
+	
+	<body>
+	
+		<h1> This is first file </h1>
+		<table id="tab" >	
+		<?php
+			//post value retrived from index page
+			$limit=$_POST['limit'];			
+			
+			for($i=1;$i<=$limit;$i++)	/* For loop for number of rows */
+			{
+				echo '<tr height="10">';
+				for($j=1;$j<=$limit;$j++)	/* For loop for number of columns */
+				{					
+					 echo '<td>';
+					 echo $i*$j;
+					 echo '</td>';
+				}	
+				echo '</tr>';		/*End loop*/
+			}
+		?>
+			
+	</table>
+	
+	</body>
+</html>
+
 
