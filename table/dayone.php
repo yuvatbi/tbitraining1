b735693cad5e66bd1a0c9e19b9c162403c2aeeb1
student <student@student-HP-Pro-3330-MT>
2012-11-20 14:17:50 +0530

    Command added

--- table/dayone.php
+++ table/dayone.php
@@ -3,16 +3,16 @@ echo '<html><head><link rel="stylesheet" href="design.css" type="text/css"/></he
 echo "This is first file";
 $limit=$_POST['limit'];
 echo '<table id="tab">';
-for($i=1;$i<=$limit;$i++)
+for($i=1;$i<=$limit;$i++)	/* For loop for number of rows */
 {
 	echo '<tr height="10">';
-	for($j=1;$j<=$limit;$j++)
+	for($j=1;$j<=$limit;$j++)	/* For loop for number of columns */
 	{
 		$a=$i*$j;
 		echo '<td>'.$a.'</td>';
 
-	}
-	echo '</tr>';
+	}	
+	echo '</tr>';				/*End loop*/
 }
 echo '</table></body></html>';
 php?>
